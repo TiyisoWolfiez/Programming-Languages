3c552b0657b6520b3408c81632407b0c572f536a
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-14 11:14:19 +0200

    Update uncleAunt.pl
    implementation of uncleAunt.pl

--- Prolog/uncleAunt.pl
+++ Prolog/uncleAunt.pl
@@ -1,1 +1,20 @@
+father(bill, jake).
+father(bill, shelley).
+father(jake, ted).
+father(ron, liz).
+mother(mary, jake).
+mother(mary, shelley).
+mother(janet, ted).
+mother(shelley, liz).
 
+uncleAunt(X, Y) :- (father(F, Y); mother(M, Y)), (father(F,X); mother(M,X)), X \= Y.
+
+% Predicate to check if two individuals are siblings
+sibling(X, Y) :-
+    father(F, X), father(F, Y), X \= Y;
+    mother(M, X), mother(M, Y), X \= Y.
+
+% Predicate to check if two individuals are cousins
+cousin(X, Y) :-
+    parent(PX, X), parent(PY, Y),
+    sibling(PX, PY), X \= Y.
