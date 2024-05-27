643c7adddb19b2102752bccb97c684a002054c93
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-27 17:57:10 +0200

    Update business_system.rb

--- Ruby/business_system.rb
+++ Ruby/business_system.rb
@@ -1,1 +1,8 @@
+class Employee
+    attr_reader :earnings
 
+    def initialize(baseSalary = 20000.00)
+        @earnings = 0.0
+        @baseSalary = baseSalary
+        @baseSalaryPaid = false
+    end
