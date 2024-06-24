7aba6707aaf84de182cd28050ee18c9f64713a07
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-24 22:16:33 +0200

    Update getPositiveOddValues.scm

--- Scheme/getPositiveOddValues.scm
+++ Scheme/getPositiveOddValues.scm
@@ -1,1 +1,21 @@
+#lang racket
 
+(define (getPositiveOddValues lst)
+   ; A helper to check if a number is positive odd                                                                                                                                                                                                                                                                            
+  (define (isPositiveOdd n)
+    (and (positive? n) (odd? n)))
+
+  ; Extracting postive odd values.
+  (if (null? lst)
+      '()
+      (let ((first-value (car lst))
+            (rest-values (cdr lst)))
+        (if (isPositiveOdd first-value)
+            (cons first-value (getPositiveOddValues rest-values))
+            (getPositiveOddValues rest-values)))))
+
+(getPositiveOddValues '())       ; Output: ()
+(getPositiveOddValues '(1 2 7 -3 4))  ; Output: (1 7)
+(getPositiveOddValues '(2 4 6))  ; Output: ()
+(getPositiveOddValues '(-1 -3)) ; Output: ()
+(getPositiveOddValues '(1 3 5))  ; Output: (1 3 5)
