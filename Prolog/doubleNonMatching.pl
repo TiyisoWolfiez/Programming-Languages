05303da8176018e85d0aceb53e7e503e2f4e08f7
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-25 06:47:14 +0200

    Update doubleNonMatching.pl

--- Prolog/doubleNonMatching.pl
+++ Prolog/doubleNonMatching.pl
@@ -1,1 +1,8 @@
-
+doubleNonMatching([],_,[]).
+doubleNonMatching([H1|T1], X, [H2|T2]) :-
+    (   H1 = X ->
+        H2 is H1,
+        doubleNonMatching(T1,X,T2)
+      ; H2 is H1 * 2,
+        doubleNonMatching(T1,X,T2)
+    ).
